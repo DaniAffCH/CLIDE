b90b8d7cd9eec7fb96bba6d4db0ba4b1fc927666
DaniAffCH <danieleaffinita2000@gmail.com>
2024-07-31 16:44:00 +0200

    teacher and student abstract

---
+++ src/feda/abstractModels/studentModel.py
@@ -0,0 +1,12 @@
+from torch.nn.modules import Module
+from feda.abstractModels.model import Model
+from abc import ABC
+import time
+
+class StudentModel(Model, ABC):
+    def __init__(self, model: Module, name: str) -> None:
+        super().__init__(model, name)
+        self.trainingStepExecuted()
+
+    def trainingStepExecuted(self) -> None:
+        self._updateTimestamp = int(round(time.time()))
