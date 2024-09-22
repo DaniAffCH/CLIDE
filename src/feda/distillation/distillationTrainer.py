08c546e0d406ba45e53885d4d58ffec687e2e313
DaniAffCH <danieleaffinita2000@gmail.com>
2024-09-22 10:34:25 +0200

    removed useless file

--- src/feda/distillation/distillationTrainer.py
+++
@@ -1,8 +0,0 @@
-from feda.abstractModel.studentModel import StudentModel
-from feda.concreteModel.teacherPool import TeacherPool
-from feda.managers.dataManager import DataManager
-
-
-class DistillationTrainer:
-    def __init__(self, studentModel: StudentModel, teacherPool: TeacherPool, dataManager: DataManager) -> None:
-        pass
